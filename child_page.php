1407f70b146538e341e9ba2949eb344aa0f84ed2
gopinath-geek <gopinath.geek@outlook.com>
2017-05-05 18:12:24 +0530

    Update child_page.php

--- child_page.php
+++ child_page.php
@@ -1,9 +1,13 @@
 <h1>Child page</h1>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
-    setInterval(function() {
-        // Send the message "Hello" to the parent window
-        // ...if the domain is still "davidwalsh.name"
-        parent.postMessage("Hello","http://localhost/test_host/index.php");
-    },1000);
+//     setInterval(function() {
+//         // Send the message "Hello" to the parent window
+//         // ...if the domain is still "davidwalsh.name"
+//         parent.postMessage("Hello","http://localhost/test_host/index.php");
+//     },1000);
+    $(document).ready(function(){
+        parent.postMessage($(document).height(), "http://localhost/test_host/index.php";
+    })
+    
 </script>
